63f4f161e1737380e27c23d728f44497a49e470e
Noah <nkasmanoff@gmail.com>
2024-09-25 13:53:53 -0400

    new llama model

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ config = {
     "MOONDREAM_MMPROJ_PATH": "../moondream-quants/moondream2-mmproj-050824-f16.gguf",
     "MOONDREAM_MODEL_PATH": "../moondream-quants/moondream2-050824-q8.gguf",
     "VISION_MODEL": "none",
-    "LOCAL_MODEL": "noahpunintended/picard:0.36b-f16",
+    "LOCAL_MODEL": "llama3.2:1b-instruct-q8_0",
     "RAG_MODEL": "gemma2:2b-instruct-q4_0",
     "STORE_CONVERSATIONS": True,  # to save in case we you want to analyze later
     "CONDENSE_MESSAGES": True,  # for faster response time
